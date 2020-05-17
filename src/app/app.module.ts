fe027fa03461e80dd8623a4c690c3684e44d94de
Padawanka <uxjovana@gmail.com>
2020-05-17 21:11:10 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRootComponent } from './app-root/app-root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRootComponent
   ],
   imports: [
     BrowserModule,
